24bf08f5a4d31bdfe2a5f5e593e74c74493d3491
David Egea Lozano <david.egea.daw@gmail.com>
2021-02-09 10:29:08 +0100

    Reorganización de Módulos

--- 03-adminpro/src/app/app.module.ts
+++ 03-adminpro/src/app/app.module.ts
@@ -2,25 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
+import { AuthModule } from './auth/auth.module';
 import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { PagesModule } from './pages/pages.module';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { PagesModule } from './pages/pages.module'
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
     NopagefoundComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PagesModule
+    PagesModule,
+    AuthModule
   ],
   bootstrap: [AppComponent]
 })
