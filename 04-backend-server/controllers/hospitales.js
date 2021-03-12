81e4cf1f9efd804b13a87c0fa60d1203543e307b
David Egea Lozano <david.egea.daw@gmail.com>
2021-03-12 10:30:21 +0100

    Crud Médicos y hospitales

--- 04-backend-server/controllers/hospitales.js
+++ 04-backend-server/controllers/hospitales.js
@@ -29,17 +29,88 @@ const crearHospital = async (req, res = response) => {
     }
    
 }
-const actualizarHospital = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'Actualizar  Hospital'
-    })
+const actualizarHospital = async (req, res = response) => {
+    try {
+        const id = req.params.id;
+        const { } = req.body;
+          const hospitalDB = await Hospital.findById(id);
+          if(!hospitalDB){
+            res.status(400).json({
+              ok: false,
+              msg: 'No existe un hospital en la BBDD'
+            })
+          }
+        
+          
+          // TODO : validar token y comprobar si es un usuario correcto
+            //Actualizar
+            const {...campos} = req.body;
+        
+            if(hospitalDB.id !==id){
+              existeHospital = await Hospital.findOne({id})
+              if(existeEmail){
+                res.status(400).json({
+                  ok: false,
+                  msg: 'Ya existe un hospital con ese id'
+                })
+              }
+            }
+          campos.id = id
+        
+            const hospitalActualizado = await Hospital.findByIdAndUpdate(id,campos , {new : true});
+        
+          res.json({
+             ok:true,
+             hospital: hospitalActualizado
+           })
+         } catch (error) {
+           console.log(error);
+           res.status(500).json({
+             ok:false,
+             msg: 'Error inesperado'
+           })
+         }   
 }
-const borrarHospital = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'Borrar Hospital'
-    })
+const borrarHospital = async (req, res = response) => {
+    try {
+        const id = req.params.id;
+        const { } = req.body;
+          const hostpitalDB = await Hospital.findById(id);
+          if(!hostpitalDB){
+            res.status(400).json({
+              ok: false,
+              msg: 'No existe un hospital en la BBDD así que no se puede borrar'
+            })
+          }
+        
+          
+          // TODO : validar token y comprobar si es un usuario correcto
+            //Borrar
+            const {password,google,...campos} = req.body;
+        
+            if(hostpitalDB.id !==id){
+              existeHospital = await Hospital.findOne({id})
+              if(existeHospital){
+                res.status(404).json({
+                  ok: false,
+                  msg: 'No se ha borrado el hospital'
+                })
+              }
+            }
+        
+            const hospitalBorrado = await Hospital.findByIdAndDelete(id);
+        
+          res.json({
+             ok:true,
+             hospital: hospitalBorrado
+           })
+         } catch (error) {
+           console.log(error);
+           res.status(500).json({
+             ok:false,
+             msg: 'Error inesperado'
+           })
+         }   
 }
 
 module.exports = {
--- 04-backend-server/controllers/medicos.js
+++ 04-backend-server/controllers/medicos.js
@@ -36,16 +36,87 @@ const crearMedico = async(req, res = response) => {
    
 }
 const actualizarMedico = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'Actualizar  Medico'
-    })
+    try {
+        const id = req.params.id;
+        const { } = req.body;
+          const medicoDB = await Medico.findById(id);
+          if(!medicoDB){
+            res.status(400).json({
+              ok: false,
+              msg: 'No existe un usuario en la BBDD'
+            })
+          }
+        
+          
+          // TODO : validar token y comprobar si es un usuario correcto
+            //Actualizar
+            const {...campos} = req.body;
+        
+            if(medicoDB.id !==id){
+              existeMedico = await Medico.findOne({id})
+              if(existeMedico){
+                res.status(400).json({
+                  ok: false,
+                  msg: 'Ya existe un usuario con ese email'
+                })
+              }
+            }
+          campos.id = id
+        
+            const medicoActualizado = await Medico.findByIdAndUpdate(id,campos , {new : true});
+        
+          res.json({
+             ok:true,
+             medico: medicoActualizado
+           })
+         } catch (error) {
+           console.log(error);
+           res.status(500).json({
+             ok:false,
+             msg: 'Error inesperado'
+           })
+         }   
 }
 const borrarMedico = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'Borrar Medico'
+     try {
+const id = req.params.id;
+const { } = req.body;
+  const medicoDB = await Medico.findById(id);
+  if(!medicoDB){
+    res.status(400).json({
+      ok: false,
+      msg: 'No existe un usuario en la BBDD así que no se puede borrar'
     })
+  }
+
+  
+  // TODO : validar token y comprobar si es un usuario correcto
+    //Borrar
+    const {...campos} = req.body;
+
+    if(medicoDB.id !==id){
+    const  existeMedico = await Medico.findOne({id})
+      if(existeMedico){
+        res.status(404).json({
+          ok: false,
+          msg: 'No se ha borrado el usuario'
+        })
+      }
+    }
+    campos.id = id;
+    const medicoBorrado = await Medico.findByIdAndDelete(id);
+
+  res.json({
+     ok:true,
+     usuario: medicoBorrado
+   })
+ } catch (error) {
+   console.log(error);
+   res.status(500).json({
+     ok:false,
+     msg: 'Error inesperado'
+   })
+ }   
 }
 
 module.exports = {
